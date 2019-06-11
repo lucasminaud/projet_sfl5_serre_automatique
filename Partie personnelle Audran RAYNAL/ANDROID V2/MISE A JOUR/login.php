1a4bcacb245fb8ef8c5454d38e3b27e17ec78f9c
AOE2244 <43984309+AOE2244@users.noreply.github.com>
2019-06-11 12:10:50 +0200

    Update login.php

--- Partie personnelle Audran RAYNAL/ANDROID V2/MISE A JOUR/login.php
+++ Partie personnelle Audran RAYNAL/ANDROID V2/MISE A JOUR/login.php
@@ -24,6 +24,21 @@ if($conn)
 	else
 	{	
 		echo "Not Connected...!";
+
 	}
 
+	$data = '{
+	"capteur": "pluvio",
+	"etat": "1"
+	}';
+
+$capt = json_decode($data);
+
+
+echo $capt->capteur;
+
+
+$url = "10.16.37.161";
+	
+	
 ?>
